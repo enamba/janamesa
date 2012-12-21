0d10cde24dda9eb00adc2189fde8da2c228c9170
Eduardo Namba <eduardo@eduardo.cc>
2012-12-21 19:22:56 -0200

    FIX Ecletica comments of order and specials meals

--- public/media/javascript/www.janamesa.com.br/006-jnm-banner.js
+++ public/media/javascript/www.janamesa.com.br/006-jnm-banner.js
@@ -1,20 +1,138 @@
-function slideSwitch() {
-    var $active = $('#slideshow LI.active');
+(function($){
+    var defaults = {
+        speed: 200,
+        delay: 4000,
+        stopOnMouseOver: false,
+        navigation: true
+    };
 
-    if ( $active.length == 0 ) $active = $('#slideshow LI:last');
+    var methods = {
+        init : function(options) {
+            return this.each(function() {
+                var $this = $(this),
+                    data = $this.data('slideshow');
 
-    var $next =  $active.next().length ? $active.next()
-        : $('#slideshow LI:first');
+                if(!data) {
+                    var settings = $.extend({}, defaults, options);
 
-    $active.addClass('last-active');
+                    $this.data('slideshow', {
+                        'speed': settings.speed,
+                        'delay': settings.delay,
+                        'stopOnMouseOver': settings.stopOnMouseOver,
+                        'navigation':settings.navigation,
+                        'timeOut':'',
+                        'isPlaying':false,
+                        'automatic': true
+                    });
+                }
 
-    $next.css({opacity: 0.0})
-        .addClass('active')
-        .animate({opacity: 1.0}, 1000, function() {
-            $active.removeClass('active last-active');
-        });
-}
+                $this.data('slideshow').timeOut = setTimeout( function() {
+                    slideSwitch($this);
+                }, $this.data('slideshow').delay);
 
-$(function() {
-    setInterval( "slideSwitch()", 6000 );
-});
+                if($this.data('slideshow').stopOnMouseOver) {
+                    $('.seletor').mouseover(function() {
+                        clearTimeout($this.data('slideshow').timeOut);
+                        $this.data('slideshow').automatic = false;
+                    });
+                    $('.seletor').mouseout(function() {
+                        $this.data('slideshow').automatic = true;
+                        $this.data('slideshow').timeOut = setTimeout( function() {
+                            slideSwitch($this);
+                        }, $this.data('slideshow').delay);
+                    });
+                }
+
+                if($this.data('slideshow').navigation) {
+                    var navigation = $('<ul class="navigation" />');
+                    $this.find('.item').each(function(index, item) {
+                        var active = (index) ? '' : 'active';
+                        navigation.append('<li class="' + active + '"></li>');
+                    });
+                    navigation.find('li').click(function() {
+                        var index = $(this).index();
+                        if(!$this.data('slideshow').isPlaying) slideSwitch($this, index);
+                    });
+                    $this.append(navigation);
+                }
+            });
+        },
+        destroy : function( ) {
+            return this.each(function() {
+                var $this = $(this);
+                clearTimeout($this.data('slideshow').timeOut);
+                $(window).unbind('.slideshow');
+                $this.removeData('slideshow');
+            });
+        }
+    };
+
+    $.fn.slideNext = function (){
+        slideSwitch(this, '', 'next');
+    }
+
+    $.fn.slidePrevius = function (){
+        slideSwitch(this, '', 'previus');
+    }
+
+    $.fn.slideDefine = function (index){
+        slideSwitch(this, index);
+    }
+
+    function slideSwitch($this, index, step) {
+        if ($this.data('slideshow').isPlaying == true){
+            return true;
+        }
+        $this.data('slideshow').isPlaying = true;
+        if ($this.data('slideshow').automatic == true){
+            clearTimeout($this.data('slideshow').timeOut);
+        }
+
+        var $active = $this.find('.item.active'),
+            $next;
+        if ($active.length==0) $active = $this.find('.item:last');
+
+        if($.isNumeric(index) && index != $active.index()) {
+            $next = $this.find('.item:eq(' + index + ')');
+        } else if (!step || step == 'next'){
+            $next = $active.next('.item').length ? $active.next('.item') : $this.find('.item:first');
+        } else {
+            $next = $active.prev('.item').length ? $active.prev('.item') : $this.find('.item:last');
+        }
+
+        $active.addClass('last-active');
+
+        if ($this.data('slideshow').automatic == true){
+            $next.css({opacity: 0})
+                .addClass('active')
+                .animate({opacity: 1}, $this.data('slideshow').speed, function() {
+                    $active.removeClass('active last-active');
+                    $this.data('slideshow').isPlaying = false;
+                        $this.data('slideshow').timeOut = setTimeout( function() {
+                             slideSwitch($this);
+                    }, $this.data('slideshow').delay);
+                });
+        } else {
+            $next.css({opacity: 0})
+                .addClass('active')
+                .animate({opacity: 1}, $this.data('slideshow').speed, function() {
+                    $active.removeClass('active last-active');
+                    $this.data('slideshow').isPlaying = false;
+                });
+        }
+
+        $this.find('.navigation li').removeClass('active');
+        $this.find('.navigation li:eq(' + $next.index() + ')').addClass('active');
+    };
+
+    $.fn.slideshow = function( method ) {
+        if ( methods[method] ) {
+            return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+        } else if ( typeof method === 'object' || ! method ) {
+            return methods.init.apply( this, arguments );
+        } else {
+            $.error( 'Method ' + method + ' does not exist on jQuery.slideshow' );
+        }
+    };
+
+})(jQuery);
--- public/media/javascript/www.janamesa.com.br/007-jnm-control.js
+++ public/media/javascript/www.janamesa.com.br/007-jnm-control.js
@@ -1,4 +1,5 @@
 $(document).ready( function () {
+    exec = false;
     $('.entrar').click(function(){
         showLoginModal();
     });
@@ -8,6 +9,20 @@ $(document).ready( function () {
     $('.entrar_modal').mouseleave(function(){
         hideLoginModal()
     });
+
+    $(document).ready(function() {
+        $('#slideshow').slideshow({
+            'speed':1000, // Fading effect speed
+            'delay':4000, // Delay between slides
+            'stopOnMouseOver': true, //Stop sliding when mouse is over
+            'navigation': true // Add navigation
+        });
+    });
+    if (!$.browser.msie) {
+        if ($(window).height() < 673) {
+            $('.footerBanner').css('top', $(window).height() - 23);
+        }
+    }
 
 //    $('.banner').mouseenter(function () {
 //        $('.seletor').css({opacity: 0.0})
@@ -26,6 +41,39 @@ $(document).ready( function () {
 });
 //window.ready(alert('ready'));
 
+$(window).scroll(function () {
+    if (!$.browser.msie) {
+        position = $(window).scrollTop() + $(window).height() - 23;
+        if (position < 560){
+            $('.footerBanner').css('top', position);
+            $('.mainContent').css('margin-top', position - 673 );
+        } else if ($(window).height() <= 570 ){
+            $('.footerBanner').css('top', 560);
+            $('.mainContent').css('margin-top', -117 );
+        } else if ($(window).height() < 673 ){
+            $('.footerBanner').css('top', $(window).height()-23);
+            $('.mainContent').css('margin-top', $(window).height() - 698 );
+        }
+    }
+});
+
+
+
+$(window).resize(function() {
+    if (!$.browser.msie) {
+        $(window).scrollTop(0);
+        if ($(window).height() < 673) {
+            position = $(window).scrollTop() + $(window).height() - 23;
+            $('.footerBanner').css('top', $(window).height() - 23);
+            $('.mainContent').css('margin-top', position - 675 );
+        } else {
+            $('.footerBanner').css('top', 675);
+            $('.mainContent').css('margin-top', 0 );
+        }
+    }
+});
+
+
 function showLoginModal () {
     $('.entrar_modal').show();
     $('.entrar').css('color', '#000');
@@ -39,4 +87,3 @@ function hideLoginModal() {
 function categorySetImage(target, imageName){
   target.src = '/media/images/jnm-frontend/category/' + imageName;
 }
-
